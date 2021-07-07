2d92cf15a3a72c74d39bcab4cfc556de5500e85f
emily883 <emily.moraima.cruz@hotmail.com>
2021-07-07 14:39:24 -0400

    Separating all the data and the paged

--- api/src/routes/beers.ts
+++ api/src/routes/beers.ts
@@ -1,20 +1,17 @@
-import { PrismaClient } from '@prisma/client';
-import { NextFunction, Request, Response, Router } from 'express';
-import paginated from '../functions/funciones';
+import { PrismaClient } from "@prisma/client";
+import { NextFunction, Request, Response, Router } from "express";
+import paginated from "../functions/funciones";
 const router = Router();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+// Make the call with /beers or with /beers?page=1 if you want it paginated
 
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  let page: any = req.query.page;
+  page = parseInt(page);
 
-
-router.get("/", async (req: Request, res:Response, next: NextFunction) => {
-    let page: any = req.query.page;
-    page = parseInt(page);
-
-    const users = await prisma.products.findMany();
-    res.json(paginated(users, page))
-})
-
-
+  const users = await prisma.products.findMany();
+  page ? res.json(paginated(users, page)) : res.json(users);
+});
 
 export default router;
