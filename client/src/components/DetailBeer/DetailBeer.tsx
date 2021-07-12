03082c9fbde55c6c2693d42d4ea2642c77fc2e70
Kowal64 <kowalczukagustin@gmail.com>
2021-07-11 23:46:26 -0300

    Ruta Detail 01

---
+++ client/src/components/DetailBeer/DetailBeer.tsx
@@ -0,0 +1,19 @@
+import React, {useState,useEffect} from "react";
+
+function DetailBeer (props){
+
+    const [detailbeer,setDetailbeer] = useState[]; 
+
+    return(
+        <div>
+            <div id= "title">
+                <h1>Nombre de la Cerveza</h1>
+            </div>
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
