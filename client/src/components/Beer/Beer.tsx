2b32bd585a81740c1f6544be29bce2e80da2a0bd
Franco Salina <francodamiansalina@gmail.com>
2021-07-25 00:26:30 -0300

    beers carousel added

--- client/src/components/Beer/Beer.tsx
+++ client/src/components/Beer/Beer.tsx
@@ -6,64 +6,64 @@ import cerveza from '../../img/cerveza.png';
 import { useDispatch } from 'react-redux';
 import { getDetail } from '../../actions';
 
-const Beer = ({ id, image, title, ibu, abv, discount,price, rating }) => {
+const Beer = ({ id, image, title, ibu, abv, discount, price, rating }) => {
 	const dispatch = useDispatch();
 	const history = useHistory();
-	
+
 	const fetchDetail = async () => {
 		await dispatch(getDetail(id));
 		history.push(`/detailBeer/${id}`);
-	  };
-	
+	};
+
 
 	return (
 		<div className={Style.containerBack}>
-		<div className={Style.container}>
-		  <button
-			onClick={fetchDetail}
-			style={{ backgroundColor: "transparent", border: "none" }}
-		  >
-			<img className={Style.image} alt="" src={image}></img>
-			<div className={Style.info}>
-		
-			  <h2 className={Style.title}>{title.toUpperCase()}</h2>
-			  <div className={Style.props}>
-				<div className={Style.bottomInfo}>
-						<div className={Style.numbers}>
-							<div className={Style.numberColum}>
-								<p className={Style.numberInColum}> IBU: </p>
-								<p className={Style.numberInColum}> {ibu} </p>
-							</div>
-							<div className={Style.numberColum}>
-								<p className={Style.numberInColum}> ABV: </p>
-								<p className={Style.numberInColum}> {abv}% </p>
+			<div className={Style.container}>
+				<button
+					onClick={fetchDetail}
+					style={{ backgroundColor: "transparent", border: "none" }}
+				>
+					<img className={Style.image} alt="" src={image}></img>
+					<div className={Style.info}>
+
+						<h2 className={Style.title}>{title.toUpperCase()}</h2>
+						<div className={Style.props}>
+							<div className={Style.bottomInfo}>
+								<div className={Style.numbers}>
+									<div className={Style.numberColum}>
+										<p className={Style.numberInColum}> IBU: </p>
+										<p className={Style.numberInColum}> {ibu} </p>
+									</div>
+									<div className={Style.numberColum}>
+										<p className={Style.numberInColum}> ABV: </p>
+										<p className={Style.numberInColum}> {abv}% </p>
+									</div>
+								</div>
+								<div className={Style.countabled}>
+									<p className={Style.stars}> {"⭐".repeat(rating)} </p>
+									{discount !== 0 ? (
+										<div className={Style.countable}>
+											<p className={Style.price}> ${(price - price * (discount / 100)).toFixed(2)} </p>
+											<div className={Style.discountInfo}>
+												<p className={Style.originalPrice}>${price}</p>
+												<p className={Style.discount}> {discount}% OFF </p>
+											</div>
+										</div>
+									) :
+										<div className={Style.countable}>
+											<p className={Style.price}>${price}</p>
+										</div>
+
+									}
+								</div>
 							</div>
 						</div>
-						<div className={Style.countabled}>
-						<p  className={Style.stars}> {"⭐".repeat(rating)} </p>
-						{discount !== 0 ? (
-							<div className={Style.countable}>
-								<p className={Style.price}> ${(price - price * (discount / 100)).toFixed(2)} </p>
-								<div  className={Style.discountInfo}>
-									<p className={Style.originalPrice}>${price}</p>
-									<p className={Style.discount}> {discount}% OFF </p>
-								</div>
-							</div>
-								) : 
-								<div className={Style.countable}>
-								<p className={Style.price}>${price}</p>
-								</div>
-
-							}
-							</div>
 					</div>
-			  </div>
+				</button>
 			</div>
-		  </button>
 		</div>
-	  </div>
 	);
-  };
+};
 
 export default Beer
 
--- client/src/components/Beers/BeersNew.tsx
+++ client/src/components/Beers/BeersNew.tsx
@@ -5,10 +5,28 @@ import { loadUsersPremium } from "../../actions";
 import './beers.css';
 import axios from "axios";
 import { useState } from "react";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Beers = () => {
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 1920, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   const dispatch = useDispatch();
-  const [news, setNews]:any=useState()
+  const [news, setNews]: any = useState()
   async function searchNews() {
     const response = await axios.get<PostValues>("http://localhost:3001/beer/news");
     setNews(response.data)
@@ -17,32 +35,32 @@ const Beers = () => {
 
 
   useEffect(() => {
-   searchNews();
+    searchNews();
   }, [dispatch]);
 
-  
+
   return (
     <div className="containerBeersPremium">
       <h1 id="titulo">Las nuevas!</h1>
-      <div className="beers">
-      {
-        Array.isArray(news)?
-         news.map((userPremium) => (
-          
-          <Beer
-            key={userPremium.id}
-            id={userPremium.id}
-            image={userPremium.image}
-            title={userPremium.title}
-            ibu={userPremium.beer.ibu}
-            abv={userPremium.beer.abv}
-            discount={userPremium.countable.discount}
-            price={userPremium.countable.price}
-            rating={userPremium.rating}
-          /> ))
-        : <h1>Cargando...</h1>
-      }
-      </div>
+      <Carousel responsive={responsive} className="beers">
+        {
+          Array.isArray(news) ?
+            news.map((userPremium) => (
+
+              <Beer
+                key={userPremium.id}
+                id={userPremium.id}
+                image={userPremium.image}
+                title={userPremium.title}
+                ibu={userPremium.beer.ibu}
+                abv={userPremium.beer.abv}
+                discount={userPremium.countable.discount}
+                price={userPremium.countable.price}
+                rating={userPremium.rating}
+              />))
+            : <h1>Cargando...</h1>
+        }
+      </Carousel>
     </div>
   );
 };
@@ -53,29 +71,29 @@ export default Beers;
 
 
 interface PostValues {
-	beer: {
-		abv: number;
-		og: number;
-		ibu: number;
-		calories: number;
-		dryHop: boolean;
-		volume: number;
-		genericType: string;
-		specificType: string;
-	};
-	infoPost: {
-		title: string;
-		description: string;
-		image: string;
-		stock: number;
-		rating: number;
-		shipping: boolean;
-		visibility: boolean;
-		username: string;
-	};
-	countable: {
-		price: number;
-		discount: number;
-	};
-	date: Date;
+  beer: {
+    abv: number;
+    og: number;
+    ibu: number;
+    calories: number;
+    dryHop: boolean;
+    volume: number;
+    genericType: string;
+    specificType: string;
+  };
+  infoPost: {
+    title: string;
+    description: string;
+    image: string;
+    stock: number;
+    rating: number;
+    shipping: boolean;
+    visibility: boolean;
+    username: string;
+  };
+  countable: {
+    price: number;
+    discount: number;
+  };
+  date: Date;
 };
