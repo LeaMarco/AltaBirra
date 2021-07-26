69bc1b84294b823803fa29e9383baca292a837e5
Lea <mmleandro@live.com.ar>
2021-07-26 13:04:59 -0300

    borrado de master

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import app from './src/app';
-
-
-app.listen(process.env.PORT, () => {
-    console.log("Corriendo en https://altabirra.herokuapp.com/");
-})
--- client/src/actions/typesName.ts
+++
@@ -1,1 +0,0 @@
-export const GET_BEERS = "GET_BEERS"
