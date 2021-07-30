e5ef5d73a061d2cbdeeee9a43218fa68734b9102
Lea <mmleandro@live.com.ar>
2021-07-30 00:30:45 -0300

    modificamos el index api

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log("Corriendo en " + process.env.HOST_FRONT);
 })
