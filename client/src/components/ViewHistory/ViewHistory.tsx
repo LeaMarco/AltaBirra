d66aedbdd818a2d47570a004593c7206117d2a02
ezequielaguilera1993 <75750236+ezequielaguilera1993@users.noreply.github.com>
2021-07-29 22:53:19 -0300

    sweet alert desactivate accountt button

--- client/src/components/ViewHistory/ViewHistory.tsx
+++ client/src/components/ViewHistory/ViewHistory.tsx
@@ -41,16 +41,6 @@ export default function ViewHistory() {
 	return (
 		<div className={Style.mainContainer}>
 			<h2>Vistos recientemente</h2>
-<<<<<<< HEAD
-			{
-				history.length
-					? <>
-						<div className={Style.cardsContainer}>
-							{history.map(post => {
-								return (
-									<div key={post.post.id} style={{ border: "1px solid black" }} className={Style.subcontainer}>
-										<Link to={`/detailBeer/${post.post.id}`} key={post.post.id} style={{ textDecoration: "none", color: "black" }}><div className={Style.detail}>
-=======
 			<div className={Style.cardsContainer}>
 				{
 					history.length
@@ -59,7 +49,6 @@ export default function ViewHistory() {
 								<div key={post.post.id} style={{ border: "1px solid black" }} className={Style.subcontainer}>
 									<Link to={`/detailBeer/${post.post.id}`} key={post.post.id} style={{ textDecoration: "none", color: "black" }}>
 										<div className={Style.detail}>
->>>>>>> d4464cc0dcaca6611ed676673c9605e4edcbc416
 											<div className={Style.subdetail}>
 												<div className={Style.imgContainer}>
 													<img src={post.post.image} alt='' className={Style.imgHistory} />
@@ -67,25 +56,6 @@ export default function ViewHistory() {
 												<div className={Style.dataContainer}>
 													<h3 className={Style.title}> {post.post.title} </h3>
 													<div className={Style.propsContainer}>
-<<<<<<< HEAD
-														<h5 className={Style.props}> IBU: ${post.post.beer.ibu} </h5>
-														<h5 className={Style.props}> ABV: ${post.post.beer.abv} </h5>
-													</div>
-												</div>
-												<div>
-													<h4> ${post.post.countable.price} </h4>
-												</div>
-											</div>
-										</div></Link>
-									</div>
-								)
-							})}
-						</div>
-						<button onClick={deleteConfirm} className={Style.Button}> Borrar historial de busqueda </button>
-					</>
-					: <h2> El historial esta vacio </h2>
-			}
-=======
 														<h5 className={Style.props}> IBU: {post.post.beer.ibu} </h5>
 														<h5 className={Style.props}> ABV: {post.post.beer.abv} </h5>
 													</div>
@@ -103,7 +73,6 @@ export default function ViewHistory() {
 				}
 			</div>
 			<button onClick={deleteConfirm} className={Style.Button}> Borrar historial de busqueda </button>
->>>>>>> d4464cc0dcaca6611ed676673c9605e4edcbc416
 		</div >
 	)
 }
